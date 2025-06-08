3df9268043554a761297f0667cb394399d15793c
Railly <railly_2009@hotmail.com>
2025-06-07 22:34:37 -0500

    chore: Add new UI components and update dependencies

---
+++ components/landing/smooth-scroll-link.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import Link, { type LinkProps } from "next/link";
+import type React from "react";
+
+interface SmoothScrollLinkProps extends LinkProps {
+	children: React.ReactNode;
+	className?: string;
+}
+
+export function SmoothScrollLink({
+	children,
+	className,
+	...props
+}: SmoothScrollLinkProps) {
+	const handleScroll = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+		e.preventDefault();
+		const href = e.currentTarget.href;
+		const targetId = href.replace(/.*#/, "");
+		const elem = document.getElementById(targetId);
+		elem?.scrollIntoView({
+			behavior: "smooth",
+		});
+	};
+
+	return (
+		<Link
+			{...props}
+			onClick={props.href.toString().startsWith("#") ? handleScroll : undefined}
+			className={className}
+		>
+			{children}
+		</Link>
+	);
+}
