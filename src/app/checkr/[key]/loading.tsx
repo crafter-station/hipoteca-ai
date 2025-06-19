2a0ec8b5e7a3e2d5e2e3fbf7f36d4af469e0259d
Anthony Cueva <hi@cueva.io>
2025-06-18 23:10:04 -0500

    feat: add loading components for Checkr and remove legacy chat components

---
+++ src/app/checkr/[key]/loading.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function CheckrNewLoading() {
+  return <div>.</div>;
+}
---
+++ src/app/checkr/loading.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function CheckrLoading() {
+  return <div>.</div>;
+}
